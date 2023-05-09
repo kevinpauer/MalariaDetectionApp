5e372cc12f404baac42c77823a62bedcf2550e67
theGrond <gohlke.max@gmx.de>
2023-05-09 14:14:17 +0200

    version with image cropper

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-import 'dart:convert';
 
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'package:tflite/tflite.dart';
 
@@ -14,55 +16,89 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Get.lazyPut(()=>ImageController());
-    return const MaterialApp(
-      // Hide the debug banner
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Image upload',
-      home: HomePage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          highlightColor: const Color(0xFFD0996F),
+          backgroundColor: const Color(0xFFFDF5EC),
+          canvasColor: const Color(0xFFFDF5EC),
+          textTheme: TextTheme(
+            headline5: ThemeData
+                .light()
+                .textTheme
+                .headline5!
+                .copyWith(color: const Color(0xFFBC764A)),
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.grey[600],
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color(0xFFBC764A),
+            centerTitle: false,
+            foregroundColor: Colors.white,
+            actionsIconTheme: IconThemeData(color: Colors.white),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateColor.resolveWith(
+                      (states) => const Color(0xFFBC764A)),
+            ),
+          ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateColor.resolveWith(
+                    (states) => const Color(0xFFBC764A),
+              ),
+              side: MaterialStateBorderSide.resolveWith(
+                      (states) => const BorderSide(color: Color(0xFFBC764A))),
+            ),
+          )),
+      home: const HomePage(title: 'Malaria Detection'),
     );
   }
 }
 
-class ImageController {
-}
+class ImageController {}
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final String title;
+
+  const HomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  File? _image;
+  XFile? _pickedFile;
+  CroppedFile? _croppedFile;
+
   List? _outputs;
   String? _result = "";
-  PickedFile? _pickedFile;
   bool _isButtonDisabled = true;
 
   @override
   void initState() {
     super.initState();
-    loadModel().then((value) {setState((){});});
+    loadModel().then((value) {
+      setState(() {});
+    });
     _isButtonDisabled = true;
   }
 
-  final _picker = ImagePicker();
-  // Implementing the image picker
-  Future<void> _pickImage() async {
-    print("Function pickImage is called!");
-    _result = "";
-    _pickedFile=
-    await _picker.getImage(source: ImageSource.gallery);
-    if (_pickedFile != null) {
-      setState(() {
-        _image = File(_pickedFile!.path);
-        _isButtonDisabled = false;
-      });
-    }
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/converted_model.tflite",
+      labels: "assets/label.txt",
+    );
   }
 
   // Start the image prediction.
@@ -74,26 +110,18 @@ class _HomePageState extends State<HomePage> {
         imageStd: 1.0,
         threshold: 0.1,
         numResults: 1,
-        asynch: true
-    );
+        asynch: true);
     var result = "malaria";
-    if (output != null && output.length != 0){
+    if (output != null && output.length != 0) {
       var temp = output[0];
       result = (temp['index'] == 1) ? "healthy" : "malaria";
     }
 
-    print("predict = "+result);
+    print("predict = " + result);
     setState(() {
       _outputs = output;
       _result = result;
     });
-  }
-
-  loadModel() async {
-    await Tflite.loadModel(
-      model: "assets/converted_model.tflite",
-      labels: "assets/label.txt",
-    );
   }
 
   @override
@@ -105,53 +133,289 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Image upload'),
+      appBar: !kIsWeb ? AppBar(title: Text(widget.title)) : null,
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (kIsWeb)
+            Padding(
+              padding: const EdgeInsets.all(kIsWeb ? 24.0 : 16.0),
+              child: Text(
+                widget.title,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .displayMedium!
+                    .copyWith(color: Theme
+                    .of(context)
+                    .highlightColor),
+              ),
+            ),
+          Expanded(child: _body()),
+        ],
+      ),
+    );
+  }
+
+  Widget _body() {
+    if (_croppedFile != null || _pickedFile != null) {
+      return _imageCard();
+    } else {
+      return _uploaderCard();
+    }
+  }
+
+  Widget _imageCard() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding:
+            const EdgeInsets.symmetric(horizontal: kIsWeb ? 24.0 : 16.0),
+            child: Card(
+              elevation: 4.0,
+              child: Padding(
+                padding: const EdgeInsets.all(kIsWeb ? 24.0 : 16.0),
+                child: _image(),
+              ),
+            ),
+          ),
+          const SizedBox(height: 24.0),
+          _menu(),
+        ],
+      ),
+    );
+  }
+
+  Widget _image() {
+    final screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    final screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+    if (_croppedFile != null) {
+      final path = _croppedFile!.path;
+      return ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: 0.8 * screenWidth,
+          maxHeight: 0.7 * screenHeight,
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(35),
-            child: Column(children: [
-              Center(
-                child: GestureDetector(
-                  child: const Text('Select An Image'),
-                  //onPressed: _openImagePicker,
-                  //onTap:()=> Get.find<ImageController>().pickImage(),
-                  onTap: ()=>_pickImage(),
+        child: kIsWeb ? Image.network(path) : Image.file(File(path)),
+      );
+    } else if (_pickedFile != null) {
+      final path = _pickedFile!.path;
+      return ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: 0.8 * screenWidth,
+          maxHeight: 0.7 * screenHeight,
+        ),
+        child: kIsWeb ? Image.network(path) : Image.file(File(path)),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  Widget _menu() {
+    return Column(
+        children:[
+        Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              _clear();
+            },
+            backgroundColor: Colors.redAccent,
+            tooltip: 'Delete',
+            child: const Icon(Icons.delete),
+          ),
+          //if (_croppedFile == null)
+          Padding(
+            padding: const EdgeInsets.only(left: 32.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                _cropImage();
+              },
+              backgroundColor: const Color(0xFFBC764A),
+              tooltip: 'Crop',
+              child: const Icon(Icons.crop),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 32.0),
+            child: FloatingActionButton(
+              onPressed: () =>
+                  classifyImage( // _isButtonDisabled ? null :
+                      File(_croppedFile!.path)),
+              backgroundColor: const Color(0xFF009256),
+              tooltip: 'Crop',
+              child: const Icon(Icons.science),
+            ),
+          ),
+
+        ]),
+        Container(
+          margin: const EdgeInsets.all(30.0),
+          child: Text('Result:',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23)),
+        ),
+        Container(
+          child: Text(_result.toString(),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23)),
+        )
+    ]);
+  }
+
+  Widget _uploaderCard() {
+    return Center(
+      child: Card(
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: SizedBox(
+          width: kIsWeb ? 380.0 : 320.0,
+          height: 300.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: DottedBorder(
+                    radius: const Radius.circular(12.0),
+                    borderType: BorderType.RRect,
+                    dashPattern: const [8, 4],
+                    color: Theme
+                        .of(context)
+                        .highlightColor
+                        .withOpacity(0.4),
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.image,
+                            color: Theme
+                                .of(context)
+                                .highlightColor,
+                            size: 80.0,
+                          ),
+                          const SizedBox(height: 24.0),
+                          Text(
+                            'Upload an image to start',
+                            style: kIsWeb
+                                ? Theme
+                                .of(context)
+                                .textTheme
+                                .headline5!
+                                .copyWith(
+                                color: Theme
+                                    .of(context)
+                                    .highlightColor)
+                                : Theme
+                                .of(context)
+                                .textTheme
+                                .bodyText2!
+                                .copyWith(
+                                color:
+                                Theme
+                                    .of(context)
+                                    .highlightColor),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(height: 35),
-              Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                height: 200,
-                color: Colors.grey[300],
-                child: _pickedFile != null
-                    ? Image.file(
-                  File(_pickedFile!
-                      .path), width: 200, height: 200, fit: BoxFit.cover,
-                )
-                    : const Text('Please select an image'),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    _uploadImage();
+                  },
+                  child: const Text('Upload'),
+                ),
               ),
-              Center(
-                  child: ElevatedButton(
-                    onPressed: _isButtonDisabled ? null : ()=> classifyImage(
-                        File(_pickedFile!.path)
-                    ),
-                    child: Text('Predict'),
-                  )
-              ),
-              Container(
-                margin: const EdgeInsets.all(30.0),
-                child: Text('Result:',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23)),
-              ),
-              Container(
-                child: Text(_result.toString(),
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23)),
-              )
-            ]),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  Future<void> _cropImage() async {
+    setState(() {
+      _result = "";
+    });
+    if (_pickedFile != null) {
+      final croppedFile = await ImageCropper().cropImage(
+        sourcePath: _pickedFile!.path,
+        compressFormat: ImageCompressFormat.jpg,
+        compressQuality: 100,
+        uiSettings: [
+          AndroidUiSettings(
+              toolbarTitle: 'Cropper',
+              toolbarColor: Colors.deepOrange,
+              toolbarWidgetColor: Colors.white,
+              initAspectRatio: CropAspectRatioPreset.original,
+              lockAspectRatio: false),
+          IOSUiSettings(
+            title: 'Cropper',
+          ),
+          WebUiSettings(
+            context: context,
+            presentStyle: CropperPresentStyle.dialog,
+            boundary: const CroppieBoundary(
+              width: 520,
+              height: 520,
+            ),
+            viewPort:
+            const CroppieViewPort(width: 480, height: 480, type: 'circle'),
+            enableExif: true,
+            enableZoom: false,
+            showZoomer: false,
+          ),
+        ],
+      );
+
+      if (croppedFile != null) {
+        setState(() {
+          _croppedFile = croppedFile;
+        });
+      }
+    }
+  }
+
+  Future<void> _uploadImage() async {
+    final pickedFile =
+    await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _pickedFile = pickedFile;
+      });
+      setState(() {
+        _croppedFile = CroppedFile(pickedFile!.path);
+      });
+    }
+  }
+
+  void _clear() {
+    setState(() {
+      _pickedFile = null;
+      _croppedFile = null;
+      _result = "";
+    });
   }
 }
